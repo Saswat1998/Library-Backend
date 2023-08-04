a88c62371d03d2d877e720dac08cc9f330c4e9ae
BugsBunny <papalipratap@gmail.com>
2023-08-04 21:47:23 +0530

    refining

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import bookRoutes from './routes/bookRoutes';
-import { sequelize } from './database/database';
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 app.use(bookRoutes)
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('books', 'root', 'saswat@1998', {
+export const sequelize = new Sequelize('books', 'root', 'saswat@1998', { // i haven't added .env file to store these sensitive info for now, for demo purpose. I understand that it should be used in production environments
     host: 'host.docker.internal', // or the actual IP address of your host machine
     dialect: 'mysql',
 });
